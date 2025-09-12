67f7f8f8658a8d43edacd0222cb38255b698c782
Geronimo-9 <gmijaresa@unicartagena.edu.co>
2025-09-12 18:26:35 -0500

    Ejercicio 1 del taller 9

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Listo");
 
 
+Persona p1 = new Persona("Carlos",25);
+p1.mostrarDetalles();
+
+Empleado e1 = new Empleado("Geronimo",20, "Marketing Digital");
+e1.mostrarDetalles();
 
 
         }
---
+++ src/Persona.java
@@ -0,0 +1,56 @@
+public class Persona {
+private String nombre;
+private int edad;
+
+public Persona(String nombre, int edad){
+    this.nombre = nombre;
+    setEdad(edad);
+
+}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        if(edad >= 0){
+            this.edad = edad;
+        }
+
+    }
+
+
+    public void mostrarDetalles(){
+
+        System.out.println("Nombre: "+nombre+" Edad: "+edad+" ");
+
+
+    }
+
+
+
+
+}
+
+class Empleado extends Persona{
+private String departamento;
+
+public Empleado(String nombre, int edad, String departamento){
+    super(nombre,edad);
+    this.departamento = departamento;
+}
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Departamento: "+departamento+" ");
+    }
+}
